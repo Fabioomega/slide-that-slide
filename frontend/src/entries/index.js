e0179856d85f81c9fcc717600316931b81a53289
Fabioomega <fabioms2002@gmail.com>
2025-11-09 17:45:08 -0300

    Improved frontend file structure and support for MPA

--- frontend/src/entries/index.js
+++ frontend/src/entries/index.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '~/style/style.css'
+import App from '~/views/App.vue'
 
 createApp(App).mount('#app')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,39 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import fs from 'fs'
+
+function getHtmlEntries(dir = 'src/pages') {
+  const result = {}
+
+  function traverse(folder) {
+    const entries = fs.readdirSync(folder, { withFileTypes: true })
+
+    for (const entry of entries) {
+      const fullPath = resolve(folder, entry.name)
+
+      if (entry.isDirectory()) {
+        traverse(fullPath)
+      } else if (entry.name.endsWith('.html')) {
+        const name = entry.name.replace('.html', '')
+        result[name] = fullPath
+      }
+    }
+  }
+
+  traverse(resolve(__dirname, dir))
+  return result
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  sever: {
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    },
+  },
+  server: {
     host: true,
     port: 5173,
     proxy: {
@@ -13,5 +42,12 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: getHtmlEntries(),
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
