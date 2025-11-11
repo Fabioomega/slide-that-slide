73714e8868cccc31628bd117ebcfc914dac03189
Ian Andriani <122290431+ianandriani07@users.noreply.github.com>
2025-11-11 16:40:03 -0300

    Adicionando comentário sobre a rota de admin
    Added a protected route for admin access to the dashboard.

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 const onlyAdmin = require('../middlewares/onlyAdmin');
 
+// Esse é um exemplo de uma rota protegida com apenas acesso do admin (pode ser deletada ao decorrer do projeto visto que é um exemplo) 
+
 router.get('/dashboard', auth, onlyAdmin, (req, res) => {
   res.json({ mensagem: 'Bem-vindo, admin!' });
 });
