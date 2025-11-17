5e7052e5f83810553fad0b83d67fb1ce0b4d6249
Fabioomega <fabioms2002@gmail.com>
2025-11-17 14:42:07 -0300

    Connected to the backend and did the styling

--- frontend/src/views/Slides.vue
+++ frontend/src/views/Slides.vue
@@ -2,6 +2,7 @@
 import { formatFrame } from "~/libraries/dinamicFormat";
 
 let currentTimer = -1;
+let source;
 
 export default {
   data() {
@@ -29,7 +30,7 @@ export default {
 
       if (
         (slide.expirationDate !== null &&
-          Data.now() > new Date(slide.expirationDate)) ||
+          Date.now() > new Date(slide.expirationDate)) ||
         slide.transitionTime <= 0
       ) {
         this.renderSlide(idx + 1);
@@ -39,12 +40,26 @@ export default {
       this.frameContent = formatFrame(slide.editorContent);
       currentTimer = setTimeout(() => this.renderSlide(idx + 1), slide.transitionTime * 1000);
     },
+    connectSSE() {
+      source = new EventSource("/api/onSlideChange");
+      source.addEventListener('slidesUpdated', (event) => {
+        this.slides = JSON.parse(event.data);
+      });
+    }
+  },
+  mounted() {
+    this.connectSSE();
   }
 }
 </script>
 
 <template>
-  <iframe :srcdoc="frameContent" sandbox=""></iframe>
+  <iframe :srcdoc="frameContent" sandbox="allow-scripts"></iframe>
 </template>
 
-<style scoped></style>
+<style scoped>
+iframe {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
